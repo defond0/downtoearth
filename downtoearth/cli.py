07cd45e3f02bdbe1191a093d69257c93c82443b3
Manoli Yiannakakis <manoli.yiannakakis@cleardata.com>
2017-02-02 18:14:33 -0800

    created sub_parsers for cleaner cli

--- downtoearth/cli.py
+++ downtoearth/cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """downtoearth creates terraform files from api configuration definitions."""
 import argparse
+import os
 
 from downtoearth.model import ApiModel
 
@@ -8,34 +9,55 @@ from downtoearth.model import ApiModel
 def parse_args():
     """Parse arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument(
+    subparsers = parser.add_subparsers(help='command')
+
+    generate_parser = subparsers.add_parser('generate', help='generate a terraform API file')
+    generate_parser.add_argument(
         '-c',
         '--composable',
         action='store_true',
         help='Modify output to permit combining with other terraform configurations'
     )
-    parser.add_argument(
-        '-d',
-        '--deploy',
+    generate_parser.add_argument(
+        '-q',
+        '--quite',
         action='store_true',
-        help='Whether to run terraform apply (and post-hooks) on the generated state file'
+        help="Don't echo the terraform"
     )
-    parser.add_argument('input')
-    parser.add_argument('output')
-    return parser.parse_args()
+    generate_parser.add_argument('input', help="dte.json configuration file")
+    generate_parser.add_argument('output', help="destination for .tf file")
+    generate_parser.set_defaults(execute_step=generate)
+
+    deploy_parser = subparsers.add_parser('deploy', help='deploy lambda code to a stage')
+    deploy_parser.add_argument('terraform', nargs='?', default=os.getcwd(), help="where the .tf lives (for reading outputs)")
+    deploy_parser.set_defaults(execute_step=deploy)
 
 
-def main():
-    """Build template and output to file."""
-    args = parse_args()
-    print(args)
+
+    args = parser.parse_args()
+    return args
+
+def generate(args):
     model = ApiModel(args)
     output = model.render_terraform()
     with open(args.output, "w") as f:
         f.write(output)
-    print(output)
-    if args.deploy:
-        model.run_terraform()
+    if not args.quite:
+        print(output)
+
+def deploy(args):
+    raise NotImplementedError("TODO: run terraform output %s (or maybe even read the state file directly)"%args.terraform)
+
+
+
+
+def main():
+    """Build template and output to file."""
+    args=parse_args()
+
+    if args.execute_step:
+        args.execute_step(args)
+
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='downtoearth',
-      version='0.3.1',
+      version='0.4.0',
       description='Utility to make API Gateway terraforms',
       author='ClearDATA',
       url='http://gitlab.eng.cleardata.com/serverless/downtoearth',
