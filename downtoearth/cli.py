b988680bc8acdfca6623c5815222c469d5ed5592
Jeff DeFond <jeff.defond@cleardata.com>
2017-02-17 12:27:24 -0800

    Add the --var option to tf run, as well as input and outputs args

--- downtoearth/cli.py
+++ downtoearth/cli.py
@@ -9,7 +9,6 @@ def parse_args():
     """Parse arguments."""
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(help='command')
-
     generate_parser = subparsers.add_parser(
         'generate',
         help='generate a terraform API file'
@@ -29,7 +28,6 @@ def parse_args():
     generate_parser.add_argument('input', help="dte.json configuration file")
     generate_parser.add_argument('output', help="destination for .tf file")
     generate_parser.set_defaults(execute_step=generate)
-
     deploy_parser = subparsers.add_parser(
         'deploy',
         help='deploy lambda code to a stage'
@@ -53,9 +51,18 @@ def parse_args():
         action='store_true',
         help="Don't echo the terraform"
     )
+    tf_parser.add_argument(
+        '-v',
+        '--var',
+        const=None,
+        help="name of varfile"
+    )
+    tf_parser.add_argument('input', help="dte.json configuration file")
+    tf_parser.add_argument('output', help="destination for .tf file")
+    tf_parser.set_defaults(execute_step=terraform_run)
     tfvar_parser = subparsers.add_parser(
         'tfvar',
-        help='apply/plan terraform changes'
+        help='create tfvar file wile lambda aliases'
     )
     tfvar_parser.add_argument('input', help="dte.json configuration file")
     tfvar_parser.add_argument('output', help="destination for .tfvar file")
@@ -77,21 +84,18 @@ def deploy(args):
 
 def terraform_var(args):
     model = ApiModel(args)
-    print(args.output)
     model.get_lambda_versions_file(args.output)
 
 def terraform_run(args):
     model = ApiModel(args)
     generate(args, model)
-    model.run_terraform()
+    model.run_terraform(args.var)
 
 def main():
     """Build template and output to file."""
     args=parse_args()
-
     if args.execute_step:
         args.execute_step(args)
-
 
 if __name__ == "__main__":
     main()
