0301c0eb7f78f2c3c5d08f0782638dc5d4b88eed
Matthew Salazar <matthew@Nova.local>
2017-06-28 14:49:13 -0500

    Updated example runme.sh with new command syntax

--- examples/runme.sh
+++ examples/runme.sh
@@ -1,9 +1,13 @@
 LAMBDA_SOURCE="./lambda_handler.py"
 LAMBDA_ZIP="./dist/api-lambda.zip"
+LAMBDA_STAGE="production"
 INPUT_JSON="./api.json"
 TERRAFORM_OUTPUT="./sample.tf"
 
 zip -r $LAMBDA_ZIP $LAMBDA_SOURCE
 
-## remove flag [--deploy] to only generate terraform without deploying resources
-downtoearth $INPUT_JSON $TERRAFORM_OUTPUT --deploy
+## Use this command to deploy your terraform
+downtoearth deploy $INPUT_JSON $LAMBDA_STAGE
+
+## Use this command to only generate the terraform without deploying
+# downtoearth generate $INPUT_JSON $TERRAFORM_OUTPUT
